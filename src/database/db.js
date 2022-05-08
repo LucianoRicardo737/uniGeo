763bd798550b067f3445c0b00425fa1ec7d20aa7
BuildTools <unconfigured@null.spigotmc.org>
2022-05-08 00:11:29 -0300

    update dependecies, and db file

--- src/database/db.js
+++ src/database/db.js
@@ -4,10 +4,7 @@ const {HOSTMONGO, DATABASE} = process.env
 const MONGODB_URL = `mongodb://${HOSTMONGO}/${DATABASE}`
 
 mongoose.connect(MONGODB_URL, {
-  useNewUrlParser:true,
-  useUnifiedTopology:true,
-  useFindAndModify:false,
-  useCreateIndex:true
+  useNewUrlParser:true
 })
   .then(console.log(`Database is connected ${DATABASE}`))
   .catch(err=>console.log(err))
